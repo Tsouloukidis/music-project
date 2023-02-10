e164fec4609a63aed034be47cec8d318d748bc59
Tsouloukidis <114908298+Tsouloukidis@users.noreply.github.com>
2023-02-10 23:36:54 +0200

    changes

--- src/stores/example-store.js
+++ src/stores/example-store.js
@@ -4,8 +4,8 @@ export const useCounterStore = defineStore('counter', {
   state: () => ({
     playList: [{
       id: 1,
-      title: 'master of puppets',
-      src: '/music/Master of Puppets (Remastered).mp3',
+      title: 'Ado',
+      src: '/music/Ado.mp3',
       duration: 0
     },
     {
@@ -13,7 +13,25 @@ export const useCounterStore = defineStore('counter', {
       title: 'fear of the dark',
       src: '/music/Iron Maiden  -  Fear of the Dark  -  Rock in Rio [High Quality].mp3',
       duration: 0
-    }]
+    },
+  {
+    id: 3,
+      title: 'Ado1',
+      src: '/music/Ado1.mp3',
+      duration: 0
+  },
+{
+  id: 4,
+  title: 'Ado2',
+  src: '/music/Ado2.mp3',
+  duration: 0
+},
+{
+  id: 5,
+  title: 'Dynamax',
+  src: '/music/Dynamax.mp3',
+  duration: 0
+}]
   }),
   getters: {
     doubleCount: (state) => state.counter * 2,
